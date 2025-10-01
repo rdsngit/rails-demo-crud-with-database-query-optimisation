c84f276e4e5b79e97e77344dd66fc4ae89868d36
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-10-01 23:08:14 +0100

    add validations to Post and Comment models for name presence and length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 255 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   has_many :comments
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 255 }
 end
