b5726e3bc113b2659b4444577d2a48baee2b022a
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-10-01 22:27:24 +0100

    style posts and comments views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: %i[ new ]
   before_action :set_comment, only: %i[ show edit update destroy ]
-
-  # GET /comments or /comments.json
-  def index
-    @comments = Comment.all
-  end
 
   # GET /comments/1 or /comments/1.json
   def show
@@ -12,7 +8,7 @@ class CommentsController < ApplicationController
 
   # GET /comments/new
   def new
-    @comment = Comment.new
+    @comment = Comment.new(post_id: params[:post_id])
   end
 
   # GET /comments/1/edit
@@ -25,7 +21,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: "Comment was successfully created." }
+        format.html { redirect_to @comment.post, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -49,16 +45,22 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/1 or /comments/1.json
   def destroy
+    post = @comment.post
     @comment.destroy!
 
     respond_to do |format|
-      format.html { redirect_to comments_path, notice: "Comment was successfully destroyed.", status: :see_other }
+      format.html { redirect_to post_path(post), notice: "Comment was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
+    def set_post
+      @post = Post.find(params.expect(:post_id))
+    end
+
     def set_comment
       @comment = Comment.find(params.expect(:id))
     end
