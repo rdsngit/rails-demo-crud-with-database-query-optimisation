12117dc0464b26a29c912fb167e42997c188da3d
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-11-06 17:11:20 +0000

    add comment model spec for testing associations and validations

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'associations' do
+    it { should belong_to(:post).counter_cache }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:content) }
+    it { should validate_length_of(:content).is_at_least(3).is_at_most(255) }
+  end
+end
