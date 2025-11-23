4cd1abe8aad8cab9785dafb68bb8f3e12b67bab5
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-11-23 20:06:43 +0000

    add before_save to upcase_first letter of Post name attribute

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true, length: { minimum: 3, maximum: 255 }
+
+  before_save do
+    if name_changed?
+      self.name = name.upcase_first
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Post, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_least(3).is_at_most(255) }
   end
+
+  describe '#before_save' do
+    context 'when :name attribute has changed and starts with a lower case letter' do
+      it 'sets the :name attribute\'s fist letter to a capital letter' do
+        post = build(:post, name: 'a new post')
+        post.save!
+        expect(post.name).to eq('A new post')
+      end
+    end
+  end
 end
