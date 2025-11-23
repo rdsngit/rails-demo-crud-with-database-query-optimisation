90cfbba3b8926969936bc36d6c0641a5b4099b05
Ryan Davidson <52106678+rdsngit@users.noreply.github.com>
2025-11-23 20:11:23 +0000

    use before_save in Comment model to set content to upcase_first letter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :post, counter_cache: true
 
   validates :content, presence: true, length: { minimum: 3, maximum: 255 }
+
+  before_save do
+    if content_changed?
+      self.content = content.upcase_first
+    end
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Comment, type: :model do
     it { should validate_presence_of(:content) }
     it { should validate_length_of(:content).is_at_least(3).is_at_most(255) }
   end
+
+  describe '#before_save' do
+    context 'when :content attribute has changed and starts with a lower case letter' do
+      it 'sets the :content attribute\'s fist letter to a capital letter' do
+        comment = build(:comment, content: 'a new comment')
+        comment.save!
+        expect(comment.content).to eq('A new comment')
+      end
+    end
+  end
 end
